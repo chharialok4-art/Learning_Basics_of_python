511bb7a032151d38776f07d0b2bdfcc6ce639d4b
Alok Chhari <chharialok4@gmail.com>
2026-05-07 18:48:04 +0530

    find_group_having_largest_sum_7/May/2026_6:47pm

--- Learning_Basics_of_python/find_group_having_largest_sum.py
+++ Learning_Basics_of_python/find_group_having_largest_sum.py
@@ -1,3 +1,18 @@
-li001 =[1,2,3,4,5,6,7,8,9];
+li001 =[[1, 10], [2, 11], [3, 12], [4, 13], [5], [6], [7], [8], [9]];
+getSum =[];
+get_max =[[0]];
 for item in li001:
-    print(item);
+    if len(item) != 2:
+        getSum.append(item);
+    else:
+        sum = item[0]+item[1];
+        item.insert(0,sum);
+        getSum.append(item);
+for item in getSum:
+    if item[0] > get_max[0][0]:
+        get_max.pop();
+        get_max.append(item);
+    else:
+        continue;
+get_max[0].pop(0);
+print(get_max[0]);
