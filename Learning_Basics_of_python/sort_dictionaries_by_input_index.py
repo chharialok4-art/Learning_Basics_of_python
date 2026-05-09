a0fed3e75fd619c6e4ee12fdc185f67d97d37ec4
Alok Chhari <chharialok4@gmail.com>
2026-05-09 12:23:45 +0530

    sorting_dictionary_9/May/2026_12:23pm

--- Learning_Basics_of_python/sort_dictionaries_by_input_index.py
+++ Learning_Basics_of_python/sort_dictionaries_by_input_index.py
@@ -3,7 +3,5 @@ test_list = [{"Gfg" : [6, 7, 8], "is" : 9, "best" : 10},
              {"Gfg" : [4, 6, 9], "is" : 16, "best" : 1}];
 initialization_key = str(input("enter the key:-"));
 get_Dict = sorted(test_list , key = lambda x : x[initialization_key], reverse = False );
-
 print(get_Dict);
-
     
---
+++ Learning_Basics_of_python/sorting_nested_dictionary.py
@@ -0,0 +1,6 @@
+test_dict = {'Nikhil' : {'English' : 5, 'Maths' :  2, 'Science' : 14},
+             'Akash' : {'English' : 15, 'Maths' :  7, 'Science' : 2},
+             'Akshat' : {'English' : 5, 'Maths' :  50, 'Science' : 20}};
+new_dict = {kys:dict(sorted(vals.items(),key = lambda x:x[1])) for kys,vals in test_dict.items()};
+
+print(new_dict);
