b4313c9116d8d1a019c88f96ec07eca14aa26175
Alok Chhari <chharialok4@gmail.com>
2026-06-06 15:19:05 +0530

    inversion_in_nested_dictionary_6/Jun/2026_3:19pm

--- Learning_Basics_of_python/inversion_in_nested_dictionary.py
+++ Learning_Basics_of_python/inversion_in_nested_dictionary.py
@@ -1,21 +1,14 @@
-test_dict = {"a" : {"b" : {"c" : {}}}, "d" : {"e" : {}},"o" : {}, "f" : {"g" : {"h" : {}}}};
-print("-------------------------------Follow this one time------------------------------------------")
-kysList = [];
-finalList =[];
-for idx,vals in test_dict.items():
-    if idx not in kysList:
-       kysList.append(idx);
-    else:
-        continue;
-    for sub_kys , sub_vals in vals.items():
-        if sub_kys not in kysList:
-            kysList.append(sub_kys);
-        else:
-            continue;
-        for sub_sub_kys , sub_sub_vals in sub_vals.items():
-            if sub_sub_kys not in kysList:
-                kysList.append(sub_sub_kys);
-            else:
-                continue;
-            finalList.append(list(kysList));
-print(finalList);
+if __name__ == "__main__":
+    temp_dict = [];
+    test_dict = {"a" : {"b" : {"c" : {}}}, "d" : {"e" : {}},"o" : {}, "f" : {"g" : {"h" : {}}}};
+    length_of_dict = 0;
+    for kys,item in test_dict.items():
+        sample_dict = {};
+        sample_dict = {kys:sample_dict};
+        for sub_kys , sub_item in item.items():
+            sample_dict = {sub_kys:sample_dict};
+            for sub_sub_kys, sub_sub_item in sub_item.items():
+                sample_dict = {sub_sub_kys:sample_dict};
+        temp_dict.append(sample_dict);
+        del sample_dict;
+print(temp_dict);
