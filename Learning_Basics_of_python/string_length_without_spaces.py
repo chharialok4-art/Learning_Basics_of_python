79b52a395ce5faa118616703532470d56cff9e13
Alok Chhari <chharialok4@gmail.com>
2026-06-11 13:25:09 +0530

    string_length_without_spaces_11/Jun/2026_1:25pm

---
+++ Learning_Basics_of_python/string_length_without_spaces.py
@@ -0,0 +1,8 @@
+str001 = "Hellow my name is alok chhari";
+print("actual string:",str001);
+get_total_len = len(str001);
+print("Actual length:",get_total_len);
+spliting_string = str001.split(" ");
+joining_splited_string = "".join(spliting_string);
+print("string without spaces:",joining_splited_string);
+print("length without spaces:",len(joining_splited_string));
