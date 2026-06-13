1496e87ca91cbc0884baa64e217a55f5e6309061
Alok Chhari <chharialok4@gmail.com>
2026-06-13 10:23:00 +0530

    search_for_vowel_13/Jun/2026_10:22am

---
+++ Learning_Basics_of_python/acccpt_string_containing_all_vowel.py
@@ -0,0 +1,17 @@
+str001 = str(input("enter the string:"));
+if "a" in str001:
+    if "e" in str001:
+        if "i" in str001:
+            if "o" in str001:
+                if "u" in str001:
+                    print("valid");
+                else:
+                    print("not valid");
+            else:
+                print("not valid");
+        else:
+            print("not valid");
+    else:
+        print("not valid");
+else:
+    print("not valid");
