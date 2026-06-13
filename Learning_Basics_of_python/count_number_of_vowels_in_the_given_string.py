3f42e663e4dfa0e3d788a68626bc6243723b102c
Alok Chhari <chharialok4@gmail.com>
2026-06-13 12:15:12 +0530

    count_number_of_vovels_in_string_13/Jun/2026_12:15pm

---
+++ Learning_Basics_of_python/count_number_of_vowels_in_the_given_string.py
@@ -0,0 +1,14 @@
+str001 = "asdfghjklqwaertyuiozexcvbnm";
+count = 0;
+count_for_a = str001.count("a");
+count = count + count_for_a;
+count_for_e = str001.count("e");
+count = count+count_for_e;
+count_for_i = str001.count("i");
+count = count + count_for_i;
+count_for_o = str001.count("o");
+count = count + count_for_o;
+count_for_u = str001.count("u");
+count = count + count_for_u;
+print(count);
+
