4f69468a160140ec2cec2cac3dbb3ce6e08c907e
Alok Chhari <chharialok4@gmail.com>
2026-06-18 07:37:09 +0530

    short_long_short.py_18/Jun/2026_7:37am

---
+++ Learning_Basics_of_python/short_long_short.py
@@ -0,0 +1,7 @@
+str001 = "hi Fucking";
+str002 = "there";
+if len(str001) < len(str002):
+    get_total = str001+" "+str002+" "+str001;
+else:
+    get_total = str002+" "+str001+" "+str002;
+print(get_total);
