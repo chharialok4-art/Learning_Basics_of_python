b943cdbcc68b19111edbdd4fe9d82bd083396cf9
Alok Chhari <chharialok4@gmail.com>
2026-08-09 10:31:31 +0530

    binary_tree.py_9/Aug/2026_10:31am

--- Learning_Basics_of_python/binary_tree.py
+++ Learning_Basics_of_python/binary_tree.py
@@ -1,13 +1,21 @@
 import math;
-searching_value = int(input("enter the number:"));
-arr001 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
-starting = 0;
-ending = len(arr001);
-mid = 0;
-while arr001[mid] != searching_value:
-    mid = int((ending+starting)/2);
-    if arr001[mid] > searching_value:
-        ending = mid;
-    else:
-        starting = mid;
+arr01 = [1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,37,39,41];
+get_value = int(input("enter the number:"));
+count = int(math.log(len(arr01),2));
+starting=0;
+ending=len(arr01);
+mid=0;
+if get_value<arr01[ending-1]:
+    for item in range(0,len(arr01),1):
+        mid=int((ending+starting)/2);
+        if arr01[mid]==get_value:
+            print("Found");
+            break;
+        elif arr01[mid]<get_value:
+            starting=mid;
+        elif arr01[mid]>get_value:
+            ending=mid;
+else:
+    print("Not Found");
+
 
