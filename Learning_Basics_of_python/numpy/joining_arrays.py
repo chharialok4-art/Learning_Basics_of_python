5dd52ef017c5bec5bef1662552a867ee2e2092e5
Alok Chhari <chharialok4@gmail.com>
2026-08-24 09:54:47 +0530

    joining_arrays.py_24/Aug/2026_9:54am

--- Learning_Basics_of_python/numpy/joining_arrays.py
+++ Learning_Basics_of_python/numpy/joining_arrays.py
@@ -15,9 +15,25 @@ print("-----------------------------------Concatenate axis=0--------------------
 make_another_concatenate_at_axis_zero=np.concatenate((array3,array4),axis=0);
 print("make_another_concatenate_at_axis_zero:",make_another_concatenate_at_axis_zero)
 print("Shape Axis-0:",make_another_concatenate_at_axis_zero.shape)
-print("--------------------------------Concatenate axis=1--------------------------------------------------")
+print("--------------------------------Concatenate axis=1-----------------------------------------")
 make_another_concatenate_at_axis_one=np.concatenate((array3,array4),axis=1);
 print("make_another_concatenate_at_axis_one:",make_another_concatenate_at_axis_one);
 print("Shape Axis-1:",make_another_concatenate_at_axis_one.shape)
+print("--------------------------------Concatenate 3D array axis=0----------------------------------")
+arr001=np.array([[[1,2,3],[100,200,300]],[[101,202,303],[121,131,141]]]);
+arr002=np.array([[["a","b","c"],["Ab","Bc","Cd"]],[["AA","BB","CC"],["001","002","003"]]])
+print("Shape arr001:",arr001.shape);
+print("Shape arr002:",arr002.shape);
+make_concatenate_arr001_and_arr002_axis_0=np.concatenate((arr001,arr002),axis=0);
+print("Shape of make_concatenate_arr001_and_arr002:",make_concatenate_arr001_and_arr002_axis_0.shape);
+print("make_concatenate_arr001_and_arr002:",make_concatenate_arr001_and_arr002_axis_0);
+print("--------------------------------Concatenate 3D array axis=1----------------------------------")
+make_concatenate_arr001_and_arr002_axis_1=np.concatenate((arr001,arr002),axis=1);
+print("Shape of make_concatenate_arr001_and_arr002:",make_concatenate_arr001_and_arr002_axis_1.shape);
+print("make_concatenate_arr001_and_arr002:",make_concatenate_arr001_and_arr002_axis_1);
+print("--------------------------------Concatenate 3D array axis=2----------------------------------")
+make_concatenate_arr001_and_arr002=np.concatenate((arr001,arr002),axis=2);
+print("Shape of make_concatenate_arr001_and_arr002:",make_concatenate_arr001_and_arr002.shape);
+print("make_concatenate_arr001_and_arr002:",make_concatenate_arr001_and_arr002);
 
 
