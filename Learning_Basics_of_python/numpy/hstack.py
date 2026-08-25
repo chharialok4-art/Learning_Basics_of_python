4a1ad33369c19c7939a8aae6c71dd6178f9b266b
Alok Chhari <chharialok4@gmail.com>
2026-08-25 21:04:26 +0530

    htack vstack_25/Aug/2026_9:04pm

--- Learning_Basics_of_python/numpy/hstack.py
+++ Learning_Basics_of_python/numpy/hstack.py
@@ -21,3 +21,10 @@ print("Shape arr001:",arr001.shape);
 print("Shape arr002:",arr002.shape);
 make_hstack=np.hstack((arr001,arr002));
 print("hstack 2D to 2D:\n",make_hstack);
+print("-------------------------------hstack 3D to 3D-------------------------------------------")
+ar01=np.array([[[1,2,3],[100,200,300]],[[101,202,303],["a","b","c"]],[["AB","CD","EF"],["A01","B02","C03"]]]);
+ar02=np.array([[["A1","B2","C3"],["A100","B200","C300"]],[["A101","B202","C303"],["1a","2b","3c"]],[["01AB","02CD","03EF"],["001A01","002B02","003C03"]]]);
+make_hstack_3D_to_3D=np.hstack((ar01,ar02));
+print("Shape make_vstack_3D_to_3D:",make_hstack_3D_to_3D.shape);
+print("make_vstack_3D_to_3D:\n",make_hstack_3D_to_3D);
+NOTE="if you give 3D array to hstack it is not match 1 to 1 of ar01 to 1 to 1 of ar02."
--- Learning_Basics_of_python/numpy/vstack.py
+++ Learning_Basics_of_python/numpy/vstack.py
@@ -15,4 +15,17 @@ print("-------------------------------vstack 2D to 1D---------------------------
 make_vstack_2D_to_1D=np.vstack((arr002,arr001));
 print("Shape make_vstack_2D_to_1D:",make_vstack_2D_to_1D.shape);
 print("make_vstack_2D_to_1D:\n",make_vstack_2D_to_1D);
+print("-------------------------------vstack 2D to 2D-------------------------------------------")
+arr01 = np.array([[4, 5, 6], [7, 8, 9]])
+arr02 = np.array([[400, 500, 600], [700, 800, 900]])
+make_vstack_2D_to_2D=np.vstack((arr01,arr02));
+print("Shape make_vstack_2D_to_2D:",make_vstack_2D_to_2D.shape)
+print("make_vstack_2D_to_2D:\n",make_vstack_2D_to_2D);
+print("-------------------------------vstack 3D to 3D-------------------------------------------")
+arr001=np.array([[[1,2,3],[100,200,300]],[[101,202,303],["a","b","c"]],[["AB","CD","EF"],["A01","B02","C03"]]]);
+arr002=np.array([[["A1","B2","C3"],["A100","B200","C300"]],[["A101","B202","C303"],["1a","2b","3c"]],[["01AB","02CD","03EF"],["001A01","002B02","003C03"]]]);
+make_vstack_3D_to_3D=np.vstack((arr001,arr002));
+print("Shape make_vstack_3D_to_3D:",make_vstack_3D_to_3D.shape);
+print("make_vstack_3D_to_3D:\n",make_vstack_3D_to_3D);
+
 
