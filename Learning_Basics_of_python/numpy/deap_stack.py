648a41cc0c8e9e178e28369d014483abfda714a4
Alok Chhari <chharialok4@gmail.com>
2026-08-25 19:03:24 +0530

    deap_stack.py_25/Aug/2026_7:03pm

--- Learning_Basics_of_python/numpy/deap_stack.py
+++ Learning_Basics_of_python/numpy/deap_stack.py
@@ -11,5 +11,9 @@ array2 = np.array([[70, 80, 90], [100, 110, 120]])
 make_dstack_2D_to_2D=np.dstack((array1,array2));
 print("Shape make_dstack_2D_to_2D:",make_dstack_2D_to_2D.shape);
 print("make_dstack_2D_to_2D:\n",make_dstack_2D_to_2D);
-
-
+print("-----------------------------------dstack 3D to 3D--------------------------------------------")
+arr001=np.array([[[1,2,3],[100,200,300]],[[101,202,303],["a","b","c"]],[["AB","CD","EF"],["A01","B02","C03"]]]);
+arr002=np.array([[["A1","B2","C3"],["A100","B200","C300"]],[["A101","B202","C303"],["1a","2b","3c"]],[["01AB","02CD","03EF"],["001A01","002B02","003C03"]]]);
+make_dstack_3D_to_3D=np.dstack((arr001,arr002));
+print("Shape make_dstack_2D_to_2D:",make_dstack_3D_to_3D.shape);
+print("make_dstack_2D_to_2D:\n",make_dstack_3D_to_3D);
