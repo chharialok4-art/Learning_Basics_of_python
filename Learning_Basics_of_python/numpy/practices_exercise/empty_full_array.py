c84d11467ca81711b7ea46a98d634afa8e4ad4bf
Alok Chhari <chharialok4@gmail.com>
2026-09-14 10:04:17 +0530

    empty_and_full_array_14/Sep/2026_10:04am

---
+++ Learning_Basics_of_python/numpy/practices_exercise/empty_full_array.py
@@ -0,0 +1,7 @@
+import numpy as np;
+print("----------------------------------Empty--------------------------------------------")
+make_empty=np.empty((3,4));
+print(make_empty);
+print("-----------------------------------full-------------------------------------")
+make_full=np.full((3,4),10);
+print(make_full);
