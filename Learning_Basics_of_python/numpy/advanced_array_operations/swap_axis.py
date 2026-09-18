2366fb5fba6a744ad3663781db18c51b1f20d73e
Alok Chhari <chharialok4@gmail.com>
2026-09-18 09:04:26 +0530

    swap_axis.py_18/Sep/2026_9:04am

--- Learning_Basics_of_python/numpy/advanced_array_operations/swap_axis.py
+++ Learning_Basics_of_python/numpy/advanced_array_operations/swap_axis.py
@@ -6,11 +6,6 @@ arr_2X3=np.swapaxes(arr_3X2,0,1);
 print("arr_2X3:\n",arr_2X3);
 print("----------------------------------Axis Swap 3X2 to 2X3---------------------------------------------------")
 print(np.swapaxes(arr_3X2,1,0));
-print("----------------------------------Axis Swap 3X2 to 2X3---------------------------------------------------")
-arr_1X4=np.arange(0,4,1).flatten();
-print("Original arr_1X4:\n",arr_1X4);
-arr_4X1=np.swapaxes(arr_1X4,1,0);
-print("Result arr_4X1:\n",arr_4X1)
-print("----------------------------------DO for 3D array---------------------------------------------------")
+
 
 
