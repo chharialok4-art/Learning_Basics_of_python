4f0c78a97d9016cae5d7c5bbb1fb43b5dc683220
Alok Chhari <chharialok4@gmail.com>
2026-09-20 04:59:09 +0530

    sum_and_prod.py_20/Sep/2026_4:56am

--- Learning_Basics_of_python/numpy/statistic/sum_and_prod.py
+++ Learning_Basics_of_python/numpy/statistic/sum_and_prod.py
@@ -18,15 +18,15 @@ print("make_sum_axis_3D_axis_zero:\n",make_sum_axis_3D_axis_Two)
 print("-----------------------------------have product 2D Array---------------------------------------------")
 print("Original:\n",ar_2D)
 make_sum=np.prod(ar_2D,axis=0);
-print("make Sum axis Zero",make_sum);
+print("make Product axis Zero",make_sum);
 make_sum_axis_one=np.prod(ar_2D,axis=1);
-print("make Sum axis One",make_sum_axis_one);
+print("make Product axis One",make_sum_axis_one);
 print("-----------------------------------have product 3D Array---------------------------------------------")
 ar_3D=np.arange(0,27,1).reshape(3,3,3);
 print("Original:\n",ar_3D);
 make_sum_axis_3D_axis_zero=np.prod(ar_3D,axis=0);
-print("make_sum_axis_3D_axis_zero:\n",make_sum_axis_3D_axis_zero)
+print("make_Product_axis_3D_axis_zero:\n",make_sum_axis_3D_axis_zero)
 make_sum_axis_3D_axis_One=np.prod(ar_3D,axis=1);
-print("make_sum_axis_3D_axis_zero:\n",make_sum_axis_3D_axis_One)
-make_sum_axis_3D_axis_Two=np.prod(ar_3D,axis=0);
-print("make_sum_axis_3D_axis_zero:\n",make_sum_axis_3D_axis_Two)
+print("make_Product_axis_3D_axis_zero:\n",make_sum_axis_3D_axis_One)
+make_sum_axis_3D_axis_Two=np.prod(ar_3D,axis=2);
+print("make_Product_axis_3D_axis_zero:\n",make_sum_axis_3D_axis_Two)
